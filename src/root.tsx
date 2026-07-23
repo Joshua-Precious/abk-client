e289faabecdc9de03921c56f197a4ff0de6b1a50
Joshua-Precious <pcjosh00@gmail.com>
2026-07-23 21:02:41 +0100

    Refactor code structure for improved readability and maintainability

--- src/root.tsx
+++ src/root.tsx
@@ -3,10 +3,14 @@ import AppRoutes from "./routes";
 import { BrowserRouter} from "react-router";
 import { createRoot } from "react-dom/client";
 import ScrollToTop from "./components/ui/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <ScrollToTop />
         <AppRoutes />
+        <Analytics />
+        <SpeedInsights />
     </BrowserRouter>
 );
