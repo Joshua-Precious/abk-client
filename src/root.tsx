8a3fe08bfba9a922b005c7863990434bb5c383e7
Joshua-Precious <pcjosh00@gmail.com>
2026-07-23 21:32:25 +0100

    fix: update import paths for Vercel analytics and speed insights components

--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,14 @@
 import "./styles/tailwind.css";
-import AppRoutes from "./routes";   
-import { BrowserRouter} from "react-router";
+import AppRoutes from "./routes";
+import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import ScrollToTop from "./components/ui/ScrollToTop";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <ScrollToTop />
-        <AppRoutes />
-        <Analytics />
-        <SpeedInsights />
-    </BrowserRouter>
+  <BrowserRouter>
+    <ScrollToTop />
+    <AppRoutes />
+    <Analytics />
+  </BrowserRouter>,
 );
