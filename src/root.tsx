7cdb1606148ecfb1f38a8597d5c5d57f50e61535
Joshua-Precious <pcjosh00@gmail.com>
2026-07-24 23:39:57 +0100

    fix: update Vercel analytics import path and add analytics dependencies

--- src/root.tsx
+++ src/root.tsx
@@ -3,7 +3,7 @@ import AppRoutes from "./routes";
 import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import ScrollToTop from "./components/ui/ScrollToTop";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
